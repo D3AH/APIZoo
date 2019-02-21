1b21c715d35d4c6123aaf7510ef6c0e209ab747c
d3ah <diegoeauyonh@gmail.com>
2019-02-20 19:18:34 -0600

    Adding update, serach and list to product controller.

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -12,7 +12,7 @@ const Product = require('../models/product');
  *
  * @param      {Object}  req     The request
  * @param      {Object}  res     The response
- * @return     {Object}  Product saved.
+ * @return     {Object}  Status error message || product saved.
  */
 function addProduct(req, res) {
     var tempProduct = new Product(req.body);
@@ -24,9 +24,8 @@ function addProduct(req, res) {
             .save()
             .then((productSaved) => {
                 productSaved ? res.status(200).send({ product: productSaved }) : res.status(400).send({ message: 'Unexpected error.' });
-            }).catch((err) => {
-                res.status(500).send({ message: 'Internal error server.' });
-            });
+            })
+            .catch((err) => res.status(500).send({ message: 'Internal error server.' }));
         });
     } else {
         res.status(400).send({ message: validate.message });
@@ -38,15 +37,57 @@ function addProduct(req, res) {
  *
  * @param      {Object}  req     The request
  * @param      {Object}  res     The response
- * @return      {String} Typeof request.
+ * @return      {String|Object}     Status error message || product deleted.
  */
 function removeProduct(req, res) {
     Product.findByIdAndDelete(req.params.id, (err, product) => {
         product ? res.status(200).send({ message: 'Product successfully deleted.', product }) : res.status(400).send({ message: 'Unexpected error. Maybe product don\'t exist.' });
     })
-    .catch((err) => {
-        res.status(500).send({ message: err });
-    });
+    .catch((err) => res.status(500).send({ message: err }));
+}
+
+/**
+ * Update a product.
+ *
+ * @param {Object} req     The request
+ * @param {Object} res     The response
+ * @returns     {String|Object}     Status error message || product updated.
+ */
+function updateProduct(req, res) {
+    Product.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, product) => {
+        product ? res.status(200).send({ product }) : res.status(500).send({ message: 'Product dont found.' });
+    })
+    .catch((err) => res.status(500).send({ err }));
+}
+
+/**
+ * Search products.
+ *
+ * @param {Object} req The request
+ * @param {Object} res The response
+ * @return String|Object|Array}     Status error message || product searched.
+ */
+function searchProduct(req, res) {
+    /**
+     * @TODO create a more complex search with multiple query params.
+     */
+    Product.find({ name: new RegExp('.*'+req.body.search+'*.', 'i') }, (err, products) => {
+        products ? res.status(200).send({ products }) : res.status(500).send({ message: 'Product dont found.' });
+    })
+    .catch((err) => res.status(500).send({ err }));
+}
+
+/**
+ * List products.
+ *
+ * @param {Object} req The request
+ * @param {Object} res The response
+ */
+function listProducts(req, res) {
+    Product.find({/* All */ }, (err, products) => {
+        res.status(200).send({ products });
+    })
+    .catch((err) => res.status(500).send({ err }));
 }
 
 module.exports = {
