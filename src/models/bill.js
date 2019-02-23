0e468003b87ca2bd9a035138c637701ef94c6ff7
d3ah <diegoeauyonh@gmail.com>
2019-02-23 08:26:13 -0600

    Adding bill model. WIP.

---
+++ src/models/bill.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+/**
+ * Bill
+ */
+const BillSchema = new mongoose.Schema({
+    /**
+     * Code of bill. [REQUIRED]
+     */
+    number: {
+        type: String,
+        index: true,
+        unique: true,
+        required: [true, 'The number is required.']
+    },
+    /**
+     * User of bill. [REQUIRED]
+     */
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'The user is required.']
+    },
+    /**
+     * Collection to buy.
+     */
+    shoping: [{
+        product: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: [true, 'The user is required.']
+        },
+        number: Number,
+    }]
+}, {
+    /**
+     * Collation, define: locale and strenght.
+     */
+    collation: { locale: 'es', strength: 1 }
+});
+
+BillSchema.index({ number: 1, name: 'text' });
+
+module.exports = mongoose.model('Bill', BillSchema);
