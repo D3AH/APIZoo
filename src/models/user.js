56d162cc24267b891d461e80a0b8a8ee7b8eab97
d3ah <diegoeauyonh@gmail.com>
2019-02-23 07:47:51 -0600

    Adding model user.

---
+++ src/models/user.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+// Mongoose types
+require('mongoose-type-email');
+
+/**
+ * User
+ */
+const UserSchema = new mongoose.Schema({
+    /**
+     * Email of user.  [REQUIRED][UNIQUE]
+     */
+    email: {
+        type: String,
+        index: true,
+        unique: true,
+        required: [true, 'The email is required.']
+    },
+    /**
+     * Name of user.  [REQUIRED]
+     */
+    name: {
+        type: String,
+        required: [true, 'The name is required.']
+    },
+    /**
+     * Surname of user. [REQUIRED]
+     */
+    surname: {
+        type: String,
+        required: [true, 'The surname is required.']
+    },
+    /**
+     * Password of user. [REQUIRED]
+     */
+    password: {
+        type: String,
+        required: [true, 'The password is required.']
+    },
+    /**
+     * Role of user.
+     */
+    role: {
+        type: String,
+        required: false,
+        default: 'ROLE_CLIENT'
+    }
+}, {
+    /**
+     * Collation, define: locale and strenght.
+     */
+    collation: { locale: 'es', strength: 1 }
+});
+
+UserSchema.index({ email: 1 });
+
+module.exports = mongoose.model('User', UserSchema);
